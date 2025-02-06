47fdce96f5db5ca23a8872aa0baf96a04fafb5ee
Jamemes <coderjames97@gmail.com>
2025-02-06 23:10:10 +0300

    2

--- newsfeedgui.lua
+++ newsfeedgui.lua
@@ -3,16 +3,12 @@ NewsFeedGui.SUSTAIN_TIME = 7
 
 Hooks:Add("LocalizationManagerPostInit", "Quickplay_Lite_loc", function(...)
 	LocalizationManager:add_localized_strings({
-		LMB = "[LMB]",
-		RMB = "[RMB]",
 		qpl_join = "Join",
 		qpl_show_all = "Show all lobbies",
 	})
 	
 	if Idstring("russian"):key() == SystemInfo:language():key() then
 		LocalizationManager:add_localized_strings({
-			LMB = "[ЛКМ]",
-			RMB = "[ПКМ]",
 			qpl_join = "Присоединится",
 			qpl_show_all = "Показать все лобби",
 		})
@@ -23,7 +19,6 @@ local function make_fine_text(text)
 	local x, y, w, h = text:text_rect()
 
 	text:set_size(w, h)
-	text:set_position(math.round(text:x()), math.round(text:y()))
 
 	return x, y, w, h
 end
@@ -32,15 +27,21 @@ function NewsFeedGui:update(t, dt)
 	if not self._lobbies then
 		return
 	end
+
 	if self._news then
 		local color = math.lerp(tweak_data.screen_colors.button_stage_2, tweak_data.screen_colors.button_stage_3, (1 + math.sin(t * 360)) / 2)
-		self._panel:child("title_announcement"):set_visible(#self._lobbies > 0 or self._news.i > 0)
-		self._panel:child("title_announcement"):set_text(string.format("%s (%s/%s)", managers.localization:to_upper_text("cn_menu_num_players_offline"), self._news.i, #self._lobbies))
-		make_fine_text(self._panel:child("title_announcement"))
-		self._panel:child("announcement_legend"):set_visible(self._panel:child("title_announcement"):visible())
-		self._panel:child("announcement_legend"):set_text(string.format("%s %s\n%s %s", managers.localization:text("LMB"), managers.localization:text("qpl_join"), managers.localization:text("RMB"), managers.localization:text("qpl_show_all")))
-		self._panel:child("announcement_legend"):set_left(self._panel:child("title_announcement"):right() + 10)
 		self._title_panel:child("title"):set_color(self._mouse_over and tweak_data.screen_colors.button_stage_2 or color)
+		
+		local title_announcement = self._panel:child("title_announcement")
+		title_announcement:set_visible(#self._lobbies > 0 or self._news.i > 0)
+		title_announcement:set_text(string.format("%s (%s/%s)", managers.localization:to_upper_text("cn_menu_num_players_offline"), self._news.i, #self._lobbies))
+
+		local announcement_legend = self._panel:child("announcement_legend")
+		announcement_legend:set_visible(self._mouse_over)
+		announcement_legend:set_text(string.format("[MOUSE 0] %s\n[MOUSE 2] %s\n[MOUSE 1] %s", managers.localization:text("qpl_join"), managers.localization:text("menu_lobby_filters"), managers.localization:text("qpl_show_all")))
+		announcement_legend:set_left(self._title_panel:right() + 10)
+		announcement_legend:set_center_y(self._title_panel:center_y())
+		
 		if self._next then
 			self._next = nil
 			self._news.i = self._news.i + 1
@@ -48,30 +49,19 @@ function NewsFeedGui:update(t, dt)
 				self._news.i = #self._lobbies > 0 and 1 or 0
 			end
 			
-			local function formating(str)
-				if str ~= "" then
-					return str .. "\n"
-				else
-					return ""
-				end
-				
+			if #self._lobbies > 0 then
+				local title_panel_text = table.concat(self._lobbies[self._news.i].lobby_info, "\n")
+				self._title_panel:child("title"):set_text(utf8.to_upper(title_panel_text))
+				local _, _, w, h = self._title_panel:child("title"):text_rect()
+				self._title_panel:child("title"):set_h(h)
+				self._title_panel:set_w(w + 10)
+				self._title_panel:set_h(h)
+				self._title_panel:set_left(self._panel:w())
+				self._title_panel:set_top(title_announcement:bottom())
+				self._present_t = t + self.PRESENT_TIME
 			end
-			
-			local owner_name = formating(self._lobbies[self._news.i].owner_name)
-			local job_name = formating(self._lobbies[self._news.i].job_name)
-			local state_name = formating(self._lobbies[self._news.i].state_name)
-			local num_plrs = string.format("[%s/4]\n", self._lobbies[self._news.i].num_plrs)
-			local difficulty = formating(self._lobbies[self._news.i].difficulty)
-			local title_panel_text = owner_name .. job_name .. state_name .. num_plrs .. difficulty
-			self._title_panel:child("title"):set_text(utf8.to_upper(title_panel_text))
-			local _, _, w, h = self._title_panel:child("title"):text_rect()
-			self._title_panel:child("title"):set_h(h)
-			self._title_panel:set_w(w + 10)
-			self._title_panel:set_h(h)
-			self._title_panel:set_left(self._panel:w())
-			self._title_panel:set_top(self._panel:child("title_announcement"):bottom())
-			self._present_t = t + self.PRESENT_TIME
 		end
+
 		if self._present_t then
 			self._title_panel:set_left(0 - (managers.gui_data:safe_scaled_size().x + self._title_panel:w()) * ((self._present_t - t) / self.PRESENT_TIME))
 			if t > self._present_t then
@@ -84,11 +74,12 @@ function NewsFeedGui:update(t, dt)
 				self._sustain_t = t + sustain
 			end
 		end
+
 		if self._sustain_t and t > self._sustain_t then
 			self._sustain_t = nil
 			self._remove_t = t + self.REMOVE_TIME
 		end
-	
+
 		if self._remove_t then
 			self._title_panel:set_left(0 - (managers.gui_data:safe_scaled_size().x + self._title_panel:w()) * (1 - (self._remove_t - t) / self.REMOVE_TIME))
 			if t > self._remove_t then
@@ -150,6 +141,7 @@ function NewsFeedGui:_create_gui()
 		name = "title_panel",
 		layer = 1
 	})
+	
 	self._title_panel:text({
 		y = 12,
 		rotation = 360,
@@ -184,19 +176,21 @@ function NewsFeedGui:make_news_request(push)
 			local job_id = tweak_data.narrative:get_job_name_from_index(math.floor(attributes_numbers[1] / 1000))
 			local state_string_id = tweak_data:index_to_server_state(attributes_numbers[4])
 			local state_name = state_string_id and managers.localization:text("menu_lobby_server_state_" .. state_string_id) or "UNKNOWN"
-			local num_plrs = attributes_numbers[5]
 			local job_tweak = job_id and tweak_data.narrative:job_data(job_id)
 			local is_professional = job_tweak and job_tweak.professional
 			local prof = is_professional and string.format(" (%s)", managers.localization:to_upper_text("cn_menu_pro_job")) or ""
+			local job_name = job_tweak and managers.localization:to_upper_text(job_tweak.name_id) .. prof
+			local difficulty = attributes_numbers[2] - 2 > 0 and string.rep("", attributes_numbers[2] - 2)
 	
 			lobbies[i] = {
 				room_id = tostring(room.room_id),
-				owner_name = tostring(room.owner_name),
-				job_name = job_tweak and managers.localization:to_upper_text(job_tweak.name_id) .. prof or "",
-				difficulty = attributes_numbers[2] - 2 > 0 and string.rep("", attributes_numbers[2] - 2) or "",
-				state_name = state_name,
-				num_plrs = num_plrs
+				lobby_info = {}
 			}
+
+			table.insert(lobbies[i].lobby_info, tostring(room.owner_name))
+			table.insert(lobbies[i].lobby_info, job_name)
+			table.insert(lobbies[i].lobby_info, tostring(state_name .. string.format(" [%s/4]", attributes_numbers[5])))
+			table.insert(lobbies[i].lobby_info, difficulty or nil)
 		end
 		
 		local function shuffle(t)
@@ -217,20 +211,24 @@ function NewsFeedGui:make_news_request(push)
 		end
 	end
 
-	managers.network.matchmake:set_distance_filter(3)
-	managers.network.matchmake:set_lobby_return_count(50)
+	if managers.network.matchmake and managers.network.matchmake.load_user_filters then
+		managers.network.matchmake:load_user_filters()
+	end
+
 	managers.network.matchmake:register_callback("search_lobby", f)
-	managers.network.matchmake:search_lobby()
+	managers.network.matchmake:search_lobby(managers.network.matchmake:search_friends_only())
 end
 
 function NewsFeedGui:mouse_pressed(button, x, y)
 	if not self._news then
 		return
 	end
+
 	if button == Idstring("0") and self._title_panel:inside(x, y) then
 		if self._lobbies[self._news.i] then
 			managers.network.matchmake:join_server_with_check(self._lobbies[self._news.i].room_id)
 		end
+		
 		return true
 	elseif button == Idstring("1") and self._title_panel:inside(x, y) then
 		if self._lobbies then
@@ -238,7 +236,7 @@ function NewsFeedGui:mouse_pressed(button, x, y)
 			for i, lobby in pairs(self._lobbies) do
 				local dialog_data = {
 					id = "quick_lobby_dialog" .. i,
-					text = string.format("%s\n%s\n%s [%s/4]\n%s", lobby.owner_name, lobby.job_name, lobby.state_name, lobby.num_plrs, lobby.difficulty)
+					text = table.concat(lobby.lobby_info, "\n")
 				}
 				local join_button = {text = managers.localization:text("cn_menu_accept_contract"), callback_func = function()
 					managers.system_menu:force_close_all()
@@ -257,6 +255,15 @@ function NewsFeedGui:mouse_pressed(button, x, y)
 				managers.system_menu:show(dialog_data)
 			end
 		end
+		
+		return true
+	elseif button == Idstring("2") then
+		managers.menu:open_node("crimenet_filters", {})
+		
+		if managers.menu_component._new_heists_gui then
+			managers.menu_component._new_heists_gui.set_bar_width = function() end
+		end
+
 		return true
 	end
 end
